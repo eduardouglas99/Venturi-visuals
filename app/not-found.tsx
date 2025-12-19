797f4139bb781233cbba40796a00e5888da04291
Eduardo <eduardouglas99@gmail.com>
2025-12-19 09:47:49 -0300

    fix: atualizando  a versao do next para uma estavel e a pagina de not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,16 +5,15 @@ import React from "react";
 export default function NotFound() {
   return (
  <section
-      style={{ backgroundImage: "linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.3)),url('/images/abstract-background.jpg')" }}
       className="flex justify-center lg:justify-start items-center h-[696px] md:h-[calc(100vh-32px)] bg-cover bg-center lg:px-24">
       <div className=" flex flex-col max-w-[calc(100%-32px)] w-full py-12 gap-12 md:py-16 md:gap-8">
         <div className="flex flex-col gap-8  md:gap-16 ">
           <div className="flex flex-col gap-6 ">
             <header className="flex flex-col gap-3 ">
-              <span className="font-semibold text-base tracking-tight">Not found</span>
-              <h1 className="text-6xl md:text-8xl mb-8 tracking-tight">We can’t find the page</h1>
+              <span className="font-semibold text-base tracking-tight">Não encontrado</span>
+              <h1 className="text-6xl md:text-8xl mb-8 tracking-tight">Não conseguimos encontrar a página.</h1>
             </header>
-            <p className="text-xl text-gray-600 max-w-3xl  leading-relaxed">Sorry, the page you are looking for doesn&apos;t  exist or has been moved.</p>
+            <p className="text-xl text-gray-600 max-w-3xl  leading-relaxed">Desculpe, a página que você está procurando não existe ou foi movida.</p>
           </div>
         </div>
       </div>
