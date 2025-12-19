7346202072817fbe99ec94fded572256836d3429
Eduardo <eduardouglas99@gmail.com>
2025-12-19 09:55:21 -0300

    fix: ajuste visual na pagina 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
  <section
       className="flex justify-center lg:justify-start items-center h-[696px] md:h-[calc(100vh-32px)] bg-cover bg-center lg:px-24">
-      <div className=" flex flex-col max-w-[calc(100%-32px)] w-full py-12 gap-12 md:py-16 md:gap-8">
+      <div className=" flex flex-col max-w-[calc(80%-32px)] w-full py-12 gap-12 md:py-16 md:gap-8">
         <div className="flex flex-col gap-8  md:gap-16 ">
           <div className="flex flex-col gap-6 ">
             <header className="flex flex-col gap-3 ">
